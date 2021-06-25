795598cbd9580de93eff8d8084297c233186e6cb
chenkunyue <1018480794@qq.com>
2021-06-25 22:12:24 +0800

    这是第一次尝试 这是第二次尝试

---
+++ src/test/java/com/itheima/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -0,0 +1,27 @@
+package com.itheima.springbootmybatis;
+
+import com.itheima.springbootmybatis.domain.User;
+import com.itheima.springbootmybatis.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class SpringbootMybatisApplicationTests {
+
+    @Autowired
+   UserMapper userMapper ;
+    @Test
+    void contextLoads() {
+        List<User> users = userMapper.queryUserList ();
+        System.out.println (users);
+    }
+
+    @Test
+    void test(){
+        System.out.println ("这是第二次尝试");
+    }
+
+}
